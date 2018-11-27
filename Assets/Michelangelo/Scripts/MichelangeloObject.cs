5b2ec27af73e0846e1ac3da0ac24907a7d100b2f
Haaxor1689 <betkomaros@gmail.com>
2018-11-28 00:08:45 +0100

    #9 Added documentation ot public MichelangeloObject methods.

--- Assets/Michelangelo/Scripts/MichelangeloObject.cs
+++ Assets/Michelangelo/Scripts/MichelangeloObject.cs
@@ -1,7 +1,6 @@
 using Michelangelo.Model;
 using Michelangelo.Model.Render;
 using RSG;
-using UnityEditor;
 using UnityEngine;
 
 namespace Michelangelo.Scripts {
@@ -30,6 +29,10 @@ namespace Michelangelo.Scripts {
             }
         }
 
+        /// <summary>
+        /// Generates new mesh for Michelangelo object asynchronously.
+        /// </summary>
+        /// <returns><see cref="IPromise"/> that contains <see cref="ModelMesh"/> and response message from server.</returns>
         public IPromise<GenerateGrammarResponse> Generate() {
             return MichelangeloSession.GenerateGrammar(Grammar.id).Then(response => CreateMesh(response.Mesh));
         }
@@ -52,6 +55,11 @@ namespace Michelangelo.Scripts {
             }
         }
 
+        /// <summary>
+        /// Constructs new <see cref="GameObject"/> with correctly initialized <see cref="MichelangeloObject"/> with a grammar.
+        /// </summary>
+        /// <param name="grammar">Grammar that newly created <see cref="MichelangeloObject"/> should be referencing.</param>
+        /// <returns>Newly created <see cref="GameObject"/> with <see cref="MichelangeloObject"/> component initialized.</returns>
         public static GameObject Construct(Grammar grammar) {
             var newObject = new GameObject(grammar.name);
             var michelangeloObject = newObject.AddComponent<MichelangeloObject>();
